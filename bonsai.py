46fa4fc36236e0f349e9770dffb4e880460817ea
Michael Folkerts <michael@target.utmroc.swmed.org>
2014-03-13 21:22:03 -0500

    first commit - control and tipsy script

---
+++ bonsai.py
@@ -0,0 +1,83 @@
+
+from subprocess import call
+import struct
+import glob
+import numpy as np
+
+BONSAI_BIN = '../runtime/bonsai2_slowdust'
+
+class Stars(object):
+
+	def __init__(self,fileObj,dim,count):
+		#pass the file pointer to this function for each particle
+		self.mass = np.zeros((count),dtype=np.float32)
+		self.pos = np.zeros((count,dim),dtype=np.float32)
+		self.vel = np.zeros((count,dim),dtype=np.float32)
+		self.metals = np.zeros((count),dtype=np.float32)
+
+		if dim == 3:
+			#3D
+			for i in range(count):
+				mass,x1,x2,x3,v1,v2,v3,metals,tform_IGNORED,eps_IGNORED,phi_IGNORED = struct.unpack('f3f3f3fi',fileObj.read(44))
+				self.pos[i,:] = (x1,x2,x3)
+				self.vel[i,:] = (v1,v2,v3)
+				self.mass[i] = mass
+				self.metals[i] = metals
+
+		else:
+			raise Exception("%iD not supported"%dim)
+
+
+
+def run_plummer(nParticles,snap_prefix,T=2,dt=0.0625):
+	bonsai_bin = '../runtime/bonsai2_slowdust'
+	log = False
+
+	if call([BONSAI_BIN,'--log' if log else '','--plummer',str(nParticles),'--snapname',snap_prefix,'--snapiter','1','-T',str(T),'-dt',str(dt)]):
+		return "Error"
+	else:
+		return "Done"
+
+def run_sphere(nParticles,snap_prefix,T=2,dt=0.0625):
+	log = False
+
+	if call([BONSAI_BIN,'--log' if log else '','--sphere',str(nParticles),'--snapname',snap_prefix,'--snapiter','1','-T',str(T),'-dt',str(dt)]):
+		return "Error"
+	else:
+		return "Done"
+
+
+
+def load_tipsy(tipsy_prefix):
+	
+	tipsy_list = glob.glob(tipsy_prefix+"*")
+
+	snaps_array = []
+
+	for tipsy_file in tipsy_list:
+		#make sure path is file
+
+		tfile = open(tipsy_file,'rb')
+
+		print 'Loading Header (%s):' % tipsy_file
+
+		#get time
+		time = struct.unpack('d',tfile.read(8)) #one double
+
+		print "Simulation Time: %i" % time
+		
+		snaps_array.append({'time':time,'gas':[],'dark':[],'star':[]})
+
+		#get number of each particle
+		nTot,nDim,nGas,nDark,nStar = struct.unpack('iiiii',tfile.read(20))
+		print "nTot: %i, nDim: %i, nGas: %i, nDark: %i, nStar: %i" % (nTot, nDim, nGas, nDark, nStar)
+
+
+
+		#process stars:
+		if nStar > 0:
+			snaps_array[-1]['star'] = Stars(tfile,nDim,nStar)
+
+		tfile.close()
+
+	return snaps_array
