314f99ba74b10dcacfa21f8dec7581bed588fa42
Michael Folkerts <michael@target.utmroc.swmed.org>
2014-03-18 21:42:31 -0500

    commenting

--- bonsai.py
+++ bonsai.py
@@ -31,9 +31,21 @@ class Stars(object):
 			raise Exception("%iD not supported"%dim)
 
 
+def run_model(model,nParticles,snap_prefix,T,dt, dSnap, bonsai_bin, mpi_n,mpi_log_file):
+	"""
+	Run a Bonsai built in model ('plummer' or 'sphere')
 
-def run_plummer(nParticles,snap_prefix,T=2,dt=0.0625, dSnap = 0.0625, bonsai_bin = None, mpi_n = 0, mpi_log_file = 'mpiout.log'):
-	
+	Keyword arguments:
+	model -- "plummer" or "sphere"
+	nParticles -- number of particles per mpi process
+	snap_prefix -- path prefix for snapshot files (time will be appended)
+	T -- total simulation time
+	dt -- internal time step
+	dSnap -- interval at which snapshot files are generated
+	bonsai_bin -- path to bonsai exe
+	mpi_n -- specifies the number of mpi processes (0 = mpi not used)
+	mpi_log_file -- single log file for mpi output (when mpi_n > 0)
+	""" 
 	if bonsai_bin is None:
 		#use default
 		bonsai_bin = BONSAI_BIN
@@ -45,7 +57,7 @@ def run_plummer(nParticles,snap_prefix,T=2,dt=0.0625, dSnap = 0.0625, bonsai_bin
 		#mpirun -n 2 --output-filename mpiout.txt ./bonsai2_slowdust -i model3_child_compact.tipsy -T1000 --logfile logfile.txt
 		if call(['mpirun','-n',str(mpi_n),
 				 '--output-filename',mpi_log_file,bonsai_bin,
-				 '--plummer',str(nParticles),
+				 '--'+model,str(nParticles),
 				 '--snapname',snap_prefix,'--snapiter',str(dSnap),
 				 '-T',str(T),'-dt',str(dt)
 				]):
@@ -55,20 +67,72 @@ def run_plummer(nParticles,snap_prefix,T=2,dt=0.0625, dSnap = 0.0625, bonsai_bin
 
 	else:
 		#single GPU mode
-		if call([bonsai_bin,'--log' if log else '','--plummer',str(nParticles),'--snapname',snap_prefix,'--snapiter',str(dSnap),'-T',str(T),'-dt',str(dt)]):
+		if call([bonsai_bin,'--log' if log else '','--'+model,str(nParticles),'--snapname',snap_prefix,'--snapiter',str(dSnap),'-T',str(T),'-dt',str(dt)]):
 			return "Error"
 		else:
 			return "Done"
 
-def run_sphere(nParticles,snap_prefix,T=2,dt=0.0625,dSnap = 0.0625):
-	log = False
 
-	if call([BONSAI_BIN,'--log' if log else '','--sphere',str(nParticles),'--snapname',snap_prefix,'--snapiter',str(dSnap),'-T',str(T),'-dt',str(dt)]):
-		return "Error"
-	else:
-		return "Done"
+def run_plummer(nParticles,snap_prefix,T=2,dt=0.0625, dSnap = 0.0625, bonsai_bin = None, mpi_n = 0, mpi_log_file = 'mpiout.log'):
+	"""
+	Run a Bonsai's built in plummer model
 
-def fig_gen(stars, index, prefix='', time=None, lim = .8, figsize = 10, pointsize = .1): #.75
+	Keyword arguments:
+	nParticles -- number of particles (per mpi process)
+	snap_prefix -- path prefix for snapshot files (time will be appended)
+	T -- total simulation time
+	dt -- internal time step
+	dSnap -- interval at which snapshot files are generated
+	bonsai_bin -- path to bonsai exe
+	mpi_n -- specifies the number of mpi processes (0 = mpi not used)
+	mpi_log_file -- single log file for mpi output (when mpi_n > 0)
+	"""
+	run_model("plummer",nParticles,snap_prefix,T,dt, dSnap, bonsai_bin, mpi_n,mpi_log_file)
+
+
+def run_sphere(nParticles,snap_prefix,T=2,dt=0.0625, dSnap = 0.0625, bonsai_bin = None, mpi_n = 0, mpi_log_file = 'mpiout.log'):
+	"""
+	Run a Bonsai's built in plummer model
+
+	Keyword arguments:
+	nParticles -- number of particles (per mpi process)
+	snap_prefix -- path prefix for snapshot files (time will be appended)
+	T -- total simulation time
+	dt -- internal time step
+	dSnap -- interval at which snapshot files are generated
+	bonsai_bin -- path to bonsai exe
+	mpi_n -- specifies the number of mpi processes (0 = mpi not used)
+	mpi_log_file -- single log file for mpi output (when mpi_n > 0)
+	"""
+	run_model("sphere",nParticles,snap_prefix,T,dt, dSnap, bonsai_bin, mpi_n,mpi_log_file)
+	
+	
+# def run_sphere(nParticles,snap_prefix,T=2,dt=0.0625,dSnap = 0.0625,):
+# 	log = False
+
+# 	if call([BONSAI_BIN,'--log' if log else '','--sphere',str(nParticles),'--snapname',snap_prefix,'--snapiter',str(dSnap),'-T',str(T),'-dt',str(dt)]):
+# 		return "Error"
+# 	else:
+# 		return "Done"
+
+def fig_gen(stars, index='', prefix='', time=None, lim = .8, figsize = 10, pointsize = .1): #.75
+	"""
+	Generates a figure ([prefix]pos_[index].png) for a given "Stars" object, usually returned in an array from load_tipsy(...) function.
+
+	Arguments:
+	stars -- an instance of the Stars() object
+
+	Keyword arguments:
+	index -- number appended to file name (default '')
+	prefix -- prepended to file name (default '')
+	time -- displayed at top of figure (default None)
+
+	Prints:
+	relative path to file just saved
+	
+	Returns:
+	void
+	"""
 		fig = plt.figure(figsize=(figsize,figsize))
 		ax = fig.gca(projection='3d')
 		ax.plot(stars.pos[:,0],stars.pos[:,1],stars.pos[:,2],'w.',markersize=pointsize)
